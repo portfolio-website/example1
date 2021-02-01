50122e9eb16a5ca0a100c5a33fdc323482db7c1f
portfolio-website <pratamyogiyogi@gmail.com>
2021-02-01 10:38:11 +0700

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,21 +65,6 @@ $(window).scroll(function () {
     }
   });
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
-
   /*--/ Star Counter /--*/
   $('.counter').counterUp({
     delay: 15,
